6d4e1dc0708d4bd1b5c3111c4a694ec51dbc8b46
Coyote <hunamusi2048@gmail.com>
2016-07-01 16:13:20 +0900

    Add files via upload

---
+++ app/client.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+var ws = new WebSocket('ws://vast-badlands-42770.herokuapp.com/server.php');
+
+$(function () {
+  $('form').submit(function(){
+    window.alert('アラートの表示');
+    var $this = $(this);
+    // ws.onopen = function() {
+    //   console.log('sent message: %s', $('#m').val());
+    // };
+    ws.send($('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  ws.onmessage = function(msg){
+    var returnObject = JSON.parse(msg.data);
+    $('#messages').append($('<li>')).append($('<span id="clientId">').text(returnObject.id)).append($('<span id="clientMessage">').text(returnObject.data));
+     
+    
+  };
+  ws.onerror = function(err){
+    console.log("err", err);
+  };
+  ws.onclose = function close() {
+    console.log('disconnected');
+  };
+});
